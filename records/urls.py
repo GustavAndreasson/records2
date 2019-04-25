83a03a61bbaa97b32fd9517ee829975cda43703b
Gustav Andreasson <gusan@balrog.ateles.se>
2019-04-25 09:54:58 +0200

    allow - in discogs username

--- records/urls.py
+++ records/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'records'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^collection/(?P<username>[\w\.]+)/get/(?P<data_level>\d+)$', views.getCollection, name='getCollection'),
-    url(r'^collection/(?P<username>[\w\.]+)/update$', views.updateCollection, name='updateCollection'),
+    url(r'^collection/(?P<username>[\w\.\-]+)/get/(?P<data_level>\d+)$', views.getCollection, name='getCollection'),
+    url(r'^collection/(?P<username>[\w\.\-]+)/update$', views.updateCollection, name='updateCollection'),
     url(r'^record/(?P<record_id>\d+)/set/(?P<listen_name>\w+)/(?P<listen_key>\w+)$', views.setRecordListen, name='setRecordListen'),
     url(r'^artist/(?P<artist_id>\d+)/get$', views.getArtist, name='getArtist'),
     url(r'^artist/(?P<artist_id>\d+)/update$', views.updateArtist, name='updateArtist'),
