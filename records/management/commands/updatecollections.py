a34f39c4b893a3356eeecc6abcb7c413b11d34e0
Gustav Andreasson <gustav.andreasson@gmail.com>
2019-05-05 13:36:12 +0200

    added updatecollections shell command

---
+++ records/management/commands/updatecollections.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from records.models import DiscogsUser
+from records.services import updateCollection
+
+class Command(BaseCommand):
+    help = "Updates collections for all users"
+
+    def handle(self, *args, **options):
+        users = DiscogsUser.objects.all()
+        for user in users:
+            self.stdout.write("Updating collection for user " + user.username)
+            updateCollection(user)
