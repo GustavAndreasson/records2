f6fdaa6e24b20544009f5dbbf7e493e518b9864e
Gustav Andreasson <gustav.andreasson@gmail.com>
2019-09-22 19:29:51 +0200

    frontend in react start

--- records2/urls.py
+++ records2/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^records/', include('records.urls')),
+    url(r'', include('frontend.urls')),
     url(r'^admin/', admin.site.urls),
 ]
