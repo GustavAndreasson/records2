39255cf5e994fb53f5e2dc73bf63918a8c12b416
Gustav Andreasson <gustav.andreasson@gmail.com>
2019-09-27 21:45:23 +0200

    records with thumbnails

--- frontend/src/components/Collection.js
+++ frontend/src/components/Collection.js
@@ -3,7 +3,7 @@ import Record from "./Record";
 
 const Collection = ({ col }) =>
     <div class="collection">
-        {col && Object.values(col).map((rec) => <Record record={rec} />)};
+      {col && Object.values(col).map((rec) => <Record record={rec, () => alert(rec.name)} />)};
     </div>
 
 export default Collection;
--- frontend/src/components/Record.js
+++ frontend/src/components/Record.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Record = ({ rec }) =>
-    (
-            <div class="record">
-            {rec.name}
-            </div>
+const Record = ({ rec, handleClick }) => {
+    const getArtists = (html) => html ?
+        rec.artists[0].artist.name :
+        rec.artists[0].artist.name;
+
+    const getFormats = () => rec.format.replace(/ /, " format-");
+    
+    return (
+            <div class="record" onclick={handleClick}>
+            <img class={'cover ${getFormats()}'} src={rec.thumbnail}
+            alt={getArtists(false) + " - " + rec.name}
+            title={getArtists(false) + " - " + rec.name} />
+        </div>
     );
+}
 export default Record;
