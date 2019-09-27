1012b05d382915af7f770497974947e3fe7e18ef
Gustav Andréasson <gustav.andreasson@ateles.se>
2019-09-27 12:25:37 +0200

    fix2

--- frontend/src/components/Collection.js
+++ frontend/src/components/Collection.js
@@ -2,9 +2,8 @@ import React from "react";
 import Record from "./Record";
 
 const Collection = ({ col }) =>
-    Object.keys(col).length > 0 ? (
-        <div class="collection">
-            Object.values(col).map((rec) => <Record record={rec} />);
-        </div>
-    ):"";
+    <div class="collection">
+        {col && Object.values(col).map((rec) => <Record record={rec} />)};
+    </div>
+
 export default Collection;
