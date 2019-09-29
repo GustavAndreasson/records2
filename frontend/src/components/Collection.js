722af39df407dea3f4a4e3e741088edeec6374da
Gustav Andreasson <gustav.andreasson@gmail.com>
2019-09-29 10:54:59 +0200

    Removed Table, correct indentation

--- frontend/src/components/Collection.js
+++ frontend/src/components/Collection.js
@@ -3,7 +3,7 @@ import Record from "./Record";
 
 const Collection = ({ col }) =>
     <div className="collection">
-      {col && Object.values(col).map((rec) => <Record rec={rec} handleClick={() => alert(rec.name)} key={rec.id} />)}
+        {col && Object.values(col).map((rec) => <Record rec={rec} handleClick={() => alert(rec.name)} key={rec.id} />)}
     </div>
 
 export default Collection;
--- frontend/src/components/Record.js
+++ frontend/src/components/Record.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const Record = ({ rec, handleClick }) => {
     const getArtists = (html) => html ?
-        rec.artists[0].artist.name :
-        rec.artists[0].artist.name;
+    rec.artists[0].artist.name :
+    rec.artists[0].artist.name;
 
     const getFormats = () => "format-" + rec.format.replace(/ /, " format-");
 
     return (
-            <div className="record" onClick={handleClick}>
+        <div className="record" onClick={handleClick}>
             <img className={`cover ${getFormats()}`} src={rec.thumbnail}
-            alt={getArtists(false) + " - " + rec.name}
-            title={getArtists(false) + " - " + rec.name} />
+                alt={getArtists(false) + " - " + rec.name}
+                title={getArtists(false) + " - " + rec.name} />
         </div>
     );
 }
