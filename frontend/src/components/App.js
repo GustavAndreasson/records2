dfe7a5b43fbe7304837cd9959fd53220fb11840d
Gustav Andreasson <gustav.andreasson@gmail.com>
2019-09-29 10:51:54 +0200

    Removed DataProvider

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import DataProvider from "./DataProvider";
 import Collection from "./Collection";
-const App = () => (
-  <DataProvider endpoint="records/collection/gustav.andreasson/get/2" 
-                render={data => <Collection col={data} />} />
-);
+
+class App extends Component {
+    state = {
+        data: [],
+        loaded: false,
+        placeholder: "Loading..."
+    };
+    componentDidMount() {
+        fetch("records/collection/gustav.andreasson/get/2")
+        .then(response => {
+            if (response.status !== 200) {
+                return this.setState({ placeholder: "Something went wrong" });
+            }
+            return response.json();
+        })
+        .then(data => this.setState({ data: data, loaded: true }));
+    }
+    render() {
+        const { data, loaded, placeholder } = this.state;
+        return loaded ? <Collection col={data} /> : placeholder;
+    }
+}
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
