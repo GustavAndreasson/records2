668bbc75804c8679afe522906ce56376eae7632e
Gustav Andreasson <gustav.andreasson@gmail.com>
2019-10-04 05:32:32 +0200

    Add tracks to popup

--- frontend/src/components/RecordPopup.js
+++ frontend/src/components/RecordPopup.js
@@ -4,13 +4,19 @@ const RecordPopup = ({ rec }) => {
     console.log(rec);
     return (rec &&
         <div className="record-popup">
-            <img className="cover" src={rec.image} />
+            <img className="cover" src={rec.cover} />
             <div className="left">
                 <div className="artists">{rec.artists[0].artist.name}</div>
                 <div className="name">{rec.name}</div>
                 <div className="format">{rec.format}</div>
                 <div className="year">{rec.year}</div>
-                <div className="tracks"></div>
+                <div className="tracks">
+                    {rec.tracks && rec.tracks.map((track) => (
+                        <div className="track" key={track.position}>
+                            <span className="position">{track.position}</span> {track.name}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
