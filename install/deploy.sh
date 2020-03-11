718f0b7bb517b6b20f1c4cb62f6102e53896e531
Gustav Andreasson <gustav.andreasson@gmail.com>
2020-03-11 11:33:53 +0100

    update deploy scripts

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rsync -avze ssh --exclude-from "$(dirname $0)/exclude_list.txt" $(dirname $0)/../ gustav@192.168.1.220:/home/gustav/records2/
+ssh gustav@192.168.1.220 'python3 records2/manage.py collectstatic'
--- install/deploy_remote.sh
+++ install/deploy_remote.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rsync -avze ssh --exclude-from "$(dirname $0)/exclude_list.txt" $(dirname $0)/../ gustav@212.85.79.94:/home/gustav/records2/
+ssh gustav@212.85.79.94 'python3 records2/manage.py collectstatic'
