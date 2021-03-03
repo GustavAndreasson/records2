c5501c53c869c50948b9ee21341e5a9a101332b8
Gustav Andreasson <gustav.andreasson@gmail.com>
2021-03-03 17:28:04 +0100

    fix deploy scripts

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-SERVER_IP=$(grep SERVER_PRIVATE_IP .env | xargs)
+SERVER_IP=$(grep SERVER_PRIVATE_IP $(dirname $0)/../.env | xargs)
 SERVER_IP=${SERVER_IP#*=}
-SERVER_USER=$(grep SERVER_USER .env | xargs)
+SERVER_USER=$(grep SERVER_USER $(dirname $0)/../.env | xargs)
 SERVER_USER=${SERVER_USER#*=}
 rsync -avze ssh --exclude-from "$(dirname $0)/exclude_list.txt" $(dirname $0)/../ $SERVER_USER@$SERVER_IP:records2/
 ssh $SERVER_USER@$SERVER_IP 'python3 records2/manage.py collectstatic'
