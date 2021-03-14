d6c71e94b8506bd73e5ca7317484216c6e3411e3
Gustav Andreasson <gustav.andreasson@gmail.com>
2021-03-14 15:54:44 +0100

    added intro text

--- frontend/src/components/App/App.component.js
+++ frontend/src/components/App/App.component.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "Components/Header";
 import Collection from "Components/Collection";
 import Progress from "Components/Progress";
@@ -16,25 +16,36 @@ const App = ({
     activeArtist,
     viewArtistCollection
 }) => (
-    <Fragment>
+    <>
         <Header />
         <Filters />
         <Orders />
         <Settings />
         { discogsUsername || (activeArtist && viewArtistCollection) ?
-            <Fragment>
+            <>
                 <ArtistInfo />
                 <Collection />
                 <RecordInfo />
                 <Progress />
-            </Fragment>
+            </>
         :
-            <Fragment>
+            <>
+                <div className="intro-text">
+                    <p>
+                        Här kan du titta på din eller andras skivsamlingar. Du kan sortera eller filtrera på flera olika attribut.
+                    </p>
+                    <p>
+                        Skriv in användarnamn från <a href="https://discogs.com">discogs.com</a> i fältet för användarnamn för att hämta en skivsamling (första gången en skivsamling hämtas kan det ta några minuter).
+                    </p>
+                    <p>
+                        Du kan också titta på en artist och vilka skivor den har släppt. Fyll då in artistens namn i fältet för artist (artisten måste finnas i databasen för att den ska komma upp som alternativ).
+                    </p>
+                </div>
                 <UsernameInput />
                 <ArtistInput />
-            </Fragment>
+            </>
         }
-    </Fragment>
+    </>
 )
 
 export default App;
