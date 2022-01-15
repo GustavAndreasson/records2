088767efb49ff0881c200f78ec80a8aa9cac8f15
Gustav Andreasson <gustav.andreasson@gmail.com>
2022-01-15 15:16:47 +0100

    Improved artist xml import

--- records/management/commands/readartistxml.py
+++ records/management/commands/readartistxml.py
@@ -10,12 +10,26 @@ class Command(BaseCommand):
         parser.add_argument('file', type=str)
 
     def handle(self, *args, **options):
+        self.stdout.write("Counting number of artists in file...")
+        with open(options['file'], 'r') as f:
+            nr_artists = sum(line.count('<artist>') for line in f)
+        self.stdout.write("Importing " + str(nr_artists) + " artists")
+        counter = 1
         context = etree.iterparse(
             options['file'], events=('end',), tag='artist')
         for event, elem in context:
             if not elem.find('data_quality').text in ('Needs Major Changes',
                                                       'Entirely Incorrect'):
                 createArtist(elem.find('id').text, elem.find('name').text)
+            counter += 1
+            progress = (counter / nr_artists) * 100
+            self.stdout.write(
+                "[{0}] {1}%".format('#'*int(progress/2)
+                                    + ' '*(50-int(progress/2)),
+                                    int(progress)),
+                ending='\r')
+            self.stdout.flush()
             elem.clear()
             while elem.getprevious() is not None:
                 del elem.getparent()[0]
+        self.stdout.write("\nArtist import done")
