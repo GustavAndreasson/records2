bedcd24116bf55a54e3892063aad7f93acf8ef87
Gustav Andreasson <gustav.andreasson@gmail.com>
2022-01-18 10:44:54 +0100

    Enable setting range on artist xml import

--- records/management/commands/readartistxml.py
+++ records/management/commands/readartistxml.py
@@ -7,29 +7,49 @@ class Command(BaseCommand):
     help = "Reads artist datadump xml and creates all artists"
 
     def add_arguments(self, parser):
-        parser.add_argument('file', type=str)
+        parser.add_argument('file', type=str,
+                            help="XML file to import")
+        parser.add_argument('-s', '--start', type=int,
+                            help="Position of first artist to import")
+        parser.add_argument('-e', '--end', type=int,
+                            help="Position of last artist to import")
 
     def handle(self, *args, **options):
         self.stdout.write("Counting number of artists in file...")
         with open(options['file'], 'r') as f:
             nr_artists = sum(line.count('<artist>') for line in f)
-        self.stdout.write("Importing " + str(nr_artists) + " artists")
-        counter = 1
+        start = options['start'] if options['start'] else 0
+        end = min(options['end'], nr_artists) if options['end'] else nr_artists
+        if start > end:
+            self.stdout.write("No artists in range")
+            return
+        if options['start'] or options['end']:
+            self.stdout.write("Importing " + str(end - start + 1) + " of "
+                              + str(nr_artists) + " artists")
+            nr_artists = end - start + 1
+        else:
+            self.stdout.write("Importing " + str(nr_artists) + " artists")
+        artist_counter = 0
+        loop_counter = 0
         context = etree.iterparse(
             options['file'], events=('end',), tag='artist')
         for event, elem in context:
-            if not elem.find('data_quality').text in ('Needs Major Changes',
-                                                      'Entirely Incorrect'):
-                createArtist(elem.find('id').text, elem.find('name').text)
-            counter += 1
-            progress = (counter / nr_artists) * 100
-            self.stdout.write(
-                "[{0}] {1}%".format('#'*int(progress/2)
-                                    + ' '*(50-int(progress/2)),
-                                    int(progress)),
-                ending='\r')
-            self.stdout.flush()
+            if loop_counter >= start:
+                if not elem.find('data_quality').text in ('Needs Major Changes',
+                                                          'Entirely Incorrect'):
+                    createArtist(elem.find('id').text, elem.find('name').text)
+                artist_counter += 1
+                progress = (artist_counter / nr_artists) * 100
+                self.stdout.write(
+                    "[{0}] {1}%".format('#'*int(progress/2)
+                                        + ' '*(50-int(progress/2)),
+                                        int(progress)),
+                    ending='\r')
+                self.stdout.flush()
             elem.clear()
             while elem.getprevious() is not None:
                 del elem.getparent()[0]
+            loop_counter += 1
+            if loop_counter > end:
+                break
         self.stdout.write("\nArtist import done")
--- records/management/commands/updaterecords.py
+++ records/management/commands/updaterecords.py
@@ -7,10 +7,12 @@ class Command(BaseCommand):
     help = "Loads extra info to all records"
 
     def add_arguments(self, parser):
-        parser.add_argument('nr', type=int)
+        parser.add_argument('limit', type=int,
+                            help="Limit number of updated records")
 
     def handle(self, *args, **options):
-        records = Record.objects.filter(updated__isnull=True)[:options['nr']]
+        records = Record.objects.filter(updated__isnull=True)[
+                                        :options['limit']]
         for record in records:
             self.stdout.write("Updating record " + record.name)
             updateRecord(record)
