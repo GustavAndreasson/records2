755bf27f81f070677718d42fbe1b811abf3a3e07
Gustav Andreasson <gustav.andreasson@gmail.com>
2022-01-20 15:03:54 +0100

    Use Suspense with i18n

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import "@babel/polyfill";
-import React from "react";
+import React, { Suspense } from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -8,7 +8,9 @@ import App from "Components/App";
 
 render(
   <Provider store={store}>
-    <App />
+    <Suspense fallback="loading">
+      <App />
+    </Suspense>
   </Provider>,
   document.getElementById("app")
 );
