2b01c98d1e1f6a369292ea04824996f95c66f435
Gustav Andreasson <gustav.andreasson@gmail.com>
2022-01-31 09:35:43 +0100

    Improve deploy scripts

--- install/deploy.sh
+++ install/deploy.sh
@@ -4,5 +4,21 @@ SERVER_IP=$(grep SERVER_PRIVATE_IP $(dirname $0)/../.env | xargs)
 SERVER_IP=${SERVER_IP#*=}
 SERVER_USER=$(grep SERVER_USER $(dirname $0)/../.env | xargs)
 SERVER_USER=${SERVER_USER#*=}
-rsync -avze ssh --exclude-from "$(dirname $0)/exclude_list.txt" $(dirname $0)/../ $SERVER_USER@$SERVER_IP:records2/
-ssh $SERVER_USER@$SERVER_IP 'records2/venv/bin/python records2/manage.py collectstatic'
+SYNC=$(rsync -avze ssh --exclude-from "$(dirname $0)/exclude_list.txt" $(dirname $0)/../ $SERVER_USER@$SERVER_IP:records2/)
+echo "$SYNC"
+if echo "$SYNC" | grep -q "requirements.txt"; then
+  echo 'records2/venv/bin/pip install -r records2/requirements.txt'
+  ssh $SERVER_USER@$SERVER_IP 'records2/venv/bin/pip install -r records2/requirements.txt'
+fi
+if echo "$SYNC" | grep -q "migrations"; then
+  echo 'records2/venv/bin/python records2/manage.py migrate'
+  ssh $SERVER_USER@$SERVER_IP 'records2/venv/bin/python records2/manage.py migrate'
+fi
+if echo "$SYNC" | grep -q "frontend"; then
+  echo 'records2/venv/bin/python records2/manage.py collectstatic'
+  ssh $SERVER_USER@$SERVER_IP 'records2/venv/bin/python records2/manage.py collectstatic'
+fi
+if echo "$SYNC" | grep -q ".py"; then
+  echo 'sudo service apache2 restart'
+  ssh -t $SERVER_USER@$SERVER_IP 'sudo service apache2 restart'
+fi
