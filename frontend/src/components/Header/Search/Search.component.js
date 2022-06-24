1f6bcc5e7757c4deab5ec9976febad79210e0915
Gustav Andreasson <gustav.andreasson@gmail.com>
2022-06-24 20:32:54 +0200

    Hide search field on mobile

--- frontend/src/components/Header/Search/Search.component.js
+++ frontend/src/components/Header/Search/Search.component.js
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Search.scss"
 
-const Search = ({ searchQuery, handleSearchUpdated }) => (
-  <div className="search">
-    <input type="text" value={searchQuery} onChange={e => handleSearchUpdated(e.target.value)} />
-    {searchQuery && (
-      <span className="clear-search fas fa-times" onClick={() => handleSearchUpdated("")}></span>
-    )}
-  </div>
-)
+const Search = ({ searchQuery, handleSearchUpdated }) => {
+  const [showSearch, setShowSearch] = useState(false)
+  const toggleShowSearch = () => setShowSearch(!showSearch)
+  return (
+    <div className={"search" + (showSearch ? " show-search" : "")}>
+      <button type="button" className="fas fa-search" onClick={toggleShowSearch}></button>
+      <input type="text" value={searchQuery} onChange={e => handleSearchUpdated(e.target.value)} />
+      {searchQuery && (
+        <span className="clear-search fas fa-times" onClick={() => handleSearchUpdated("")}></span>
+      )}
+    </div>
+  )
+}
 
 export default Search
