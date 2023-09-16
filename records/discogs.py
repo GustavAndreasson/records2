27df3f5d05aeb0e229fd4b7155fe2642c68ce9dc
Gustav Andreasson <gustav.andreasson@gmail.com>
2023-09-16 16:17:08 +0200

    Improved get paginated collection code

--- records/discogs.py
+++ records/discogs.py
@@ -21,8 +21,9 @@ class DiscogsError(Exception):
 
 
 def getCollection(user_name):
-    return __getPaginatedCollection("users/" + user_name
-                                    + "/collection/folders/0/releases")
+    return __getPaginatedCollection(
+        "users/" + user_name + "/collection/folders/0/releases"
+    )
 
 
 def getRelease(release_id):
@@ -38,49 +39,69 @@ def getArtist(artist_id):
 
 
 def getArtistReleases(artist_id, check_main=True):
-    return __getPaginatedCollection("/artists/" + str(artist_id)
-                                    + "/releases", check_main)
+    return __getPaginatedCollection(
+        "/artists/" + str(artist_id) + "/releases", check_main
+    )
 
 
 def __getPaginatedCollection(uri, check_main=False):
-    page = 1
+    page = 0
+    pages = 100
     collection = []
-    progress.updateProgress('discogs', 0)
+    progress.updateProgress("discogs", 0)
     try:
-        response = __readUri(uri + "?per_page=25&page=" + str(page))
-        collection.extend(response['releases'])
-        while response['pagination']['pages'] > page:
-            progress.updateProgress('discogs', int(
-                (page * 100) / response['pagination']['pages']))
+        while True:
+            progress.updateProgress("discogs", int((page * 100) / pages))
             page = page + 1
             response = __readUri(uri + "?per_page=25&page=" + str(page))
-            collection.extend(response['releases'])
-            if check_main and response['releases'][-1].get('role') != "Main":
+            if response["pagination"]["items"] == 0:
+                break
+            releases = response.get("releases")
+            if releases:
+                collection.extend(releases)
+            else:
+                raise DiscogsError(
+                    "KeyError",
+                    "Collection page did not contain releases. "
+                    + uri
+                    + "?per_page=25&page="
+                    + str(page),
+                )
+            pages = response["pagination"]["pages"]
+            if pages <= page:
+                break
+            if check_main and response["releases"][-1].get("role") != "Main":
                 break
     except DiscogsError as de:
-        logger.error(
-            "Not expected collection response from Discogs:\n" + str(de))
+        logger.error("Not expected collection response from Discogs:\n" + str(de))
         if not collection:
             raise
-    progress.updateProgress('discogs', 100)
+    progress.updateProgress("discogs", 100)
     return collection
 
 
 def __readUri(uri):
     accesses = DiscogsAccess.objects.filter(
-        timestamp__gt=int(time.time()) - time_discogs_accesses - 1)
+        timestamp__gt=int(time.time()) - time_discogs_accesses - 1
+    )
     if len(accesses) >= max_discogs_accesses - 1:
-        wait = max(time_discogs_accesses + 1
-                   - (int(time.time()) - accesses[0].timestamp), 0)
-        logger.debug("Limit reached on discogs accesses with "
-                     + str(len(accesses)) + " in last "
-                     + str(time_discogs_accesses) + " seconds. Waiting "
-                     + str(wait) + " seconds")
+        wait = max(
+            time_discogs_accesses + 1 - (int(time.time()) - accesses[0].timestamp), 0
+        )
+        logger.debug(
+            "Limit reached on discogs accesses with "
+            + str(len(accesses))
+            + " in last "
+            + str(time_discogs_accesses)
+            + " seconds. Waiting "
+            + str(wait)
+            + " seconds"
+        )
         time.sleep(wait)
     DiscogsAccess.objects.create(timestamp=time.time())
-    headers = {"User-Agent": config('DISCOGS_AGENT')}
-    params = {"key": config('DISCOGS_KEY'), "secret": config('DISCOGS_SECRET')}
-    url = config('DISCOGS_BASE_URL') + uri
+    headers = {"User-Agent": config("DISCOGS_AGENT")}
+    params = {"key": config("DISCOGS_KEY"), "secret": config("DISCOGS_SECRET")}
+    url = config("DISCOGS_BASE_URL") + uri
     try:
         r = requests.get(url, params=params, headers=headers)
         data = r.json()
@@ -89,14 +110,18 @@ def __readUri(uri):
     except JSONDecodeError as je:
         raise DiscogsError(r.status_code, str(je))
     if r.status_code == 429:
-        logger.error("Too many requests to Discogs\n" + data.get('message')
-                     + "\ntrying again after " + str(time_discogs_accesses)
-                     + " seconds")
+        logger.error(
+            "Too many requests to Discogs\n"
+            + data.get("message")
+            + "\ntrying again after "
+            + str(time_discogs_accesses)
+            + " seconds"
+        )
         time.sleep(time_discogs_accesses)
         r = requests.get(url, params=params, headers=headers)
     elif r.status_code != 200:
         message = "Error calling " + url
-        if data.get('error'):
-            message = data['error'].get('message')
+        if data.get("error"):
+            message = data["error"].get("message")
         raise DiscogsError(r.status_code, message)
     return data
