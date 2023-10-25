6a4f47260a870a07bbb78020565c68d1505fcb46
Gustav Andreasson <gustav.andreasson@gmail.com>
2023-10-25 11:43:37 +0200

    Validate input of getRate

--- records/views/rates.py
+++ records/views/rates.py
@@ -4,11 +4,10 @@ import records.services.rates as ratesService
 
 
 def getRate(request, currency):
+    if not (currency.isupper() and len(currency) == 3):
+        return Http404
     rate = ratesService.getRate(currency)
     if not rate:
         return Http404
     else:
-        return HttpResponse(json.dumps({
-            "currency": currency,
-            "rate": rate
-        }))
+        return HttpResponse(json.dumps({"currency": currency, "rate": rate}))
