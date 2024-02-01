18033a27ebb48105f8bdadc6dbe6436f9dc43264
Gustav Andreasson <gustav.andreasson@gmail.com>
2024-02-01 23:14:27 +0100

    Improve spotify and currency requests code

--- records/services/rates.py
+++ records/services/rates.py
@@ -1,4 +1,5 @@
 import requests
+from json.decoder import JSONDecodeError
 from decouple import config
 from django.core.cache import cache
 import time
@@ -11,17 +12,20 @@ def getRate(currency: str) -> int | None:
     yesterday = time.time() - 86400
     if not rates or rates.get("query").get("timestamp") < yesterday:
         url = str(config("RATES_API_URL") + config("RATES_API_KEY"))
-        r = requests.get(url)
-        if r.status_code == 200:
-            data = r.json()
-            rates = data
-            cache.set(RATES_CACHE_KEY, rates)
-        else:
-            return None
+        try:
+            r = requests.get(url)
+            if r.status_code == 200:
+                data = r.json()
+                rates = data
+                cache.set(RATES_CACHE_KEY, rates)
+            elif not rates:
+                return None
+        except (requests.exceptions.RequestException, JSONDecodeError):
+            if not rates:
+                return None
     if currency == rates.get("query").get("base_currency"):
         return 1
     rate = rates.get("data").get(currency)
-    if rate:
-        return rate
-    else:
+    if not rate:
         return None
+    return rate
--- records/spotify.py
+++ records/spotify.py
@@ -2,6 +2,7 @@ import requests
 import base64
 import time
 import logging
+import json
 from json.decoder import JSONDecodeError
 from urllib.parse import quote
 from django.core.cache import cache
@@ -14,37 +15,45 @@ TOKEN_KEY = "spotify_token_cache_key"
 class SpotifyError(Exception):
     def __init__(self, code, message):
         self.code = code
+        try:
+            data = json.loads(message)
+            error = data.get("error")
+            message = (error and error.get("message")) or message
+        except JSONDecodeError:
+            pass
         self.message = message
 
     def __str__(self):
         return str(self.code) + ":" + self.message
 
 
-def getAlbum(artist, album):
+def getAlbum(artist: str, album: str) -> dict | None:
     auth = "Bearer " + __getToken()
     headers = {"Authorization": auth}
     query = "album:" + quote(album) + "%20artist:" + quote(artist) + "&type=album"
-    r = requests.get(str(config("SPOTIFY_API_URL")) + "search?q=" + query, headers=headers)
     try:
+        r = requests.get(str(config("SPOTIFY_API_URL")) + "search?q=" + query, headers=headers)
+        if r.status_code != 200:
+            raise SpotifyError(r.status_code, r.text)
         data = r.json()
-    except JSONDecodeError:
-        data = {}
-    if r.status_code == 200:
-        if data["albums"]["total"] > 0:
-            return data["albums"]["items"][0]
-    elif r.status_code >= 400:
-        raise SpotifyError(r.status_code, data["error"].get("message"))
-    return None
+    except requests.exceptions.RequestException as re:
+        raise SpotifyError(re.response.status_code if re.response is not None else -1, str(re))
+    except JSONDecodeError as je:
+        raise SpotifyError(-1, je.msg)
+    albums = data.get("albums")
+    if not albums or albums.get("total") <= 0:
+        return None
+    return albums["items"][0]
 
 
-def getAlbumId(artist, album):
+def getAlbumId(artist: str, album: str) -> str | None:
     album_data = getAlbum(artist, album)
-    if album_data:
-        return album_data["id"]
-    return None
+    if not album_data:
+        return None
+    return album_data["id"]
 
 
-def __getToken():
+def __getToken() -> str:
     token_info = cache.get(TOKEN_KEY)
     if not token_info or time.time() > token_info[1]:
         token_info = __renewToken()
@@ -52,11 +61,19 @@ def __getToken():
     return token_info[0]
 
 
-def __renewToken():
+def __renewToken() -> tuple[str, float]:
     auth = base64.b64encode(bytes(str(config("SPOTIFY_ID")) + ":" + str(config("SPOTIFY_SECRET")), "utf-8"))
     headers = {"Authorization": "Basic " + auth.decode(), "Content-Type": "application/x-www-form-urlencoded"}
     data = {"grant_type": "client_credentials"}
-    r = requests.post(str(config("SPOTIFY_ACCOUNT_URL")) + "token", data=data, headers=headers)
-    token_info = (r.json()["access_token"], r.json()["expires_in"] + time.time())
+    try:
+        r = requests.post(str(config("SPOTIFY_ACCOUNT_URL")) + "token", data=data, headers=headers)
+        if r.status_code != 200:
+            raise SpotifyError(r.status_code, r.text)
+        data = r.json()
+    except requests.exceptions.RequestException as re:
+        raise SpotifyError(re.response.status_code if re.response is not None else -1, str(re))
+    except JSONDecodeError as je:
+        raise SpotifyError(-1, je.msg)
+    token_info = (data.get("access_token"), data.get("expires_in") + time.time())
     cache.set(TOKEN_KEY, token_info)
     return token_info
